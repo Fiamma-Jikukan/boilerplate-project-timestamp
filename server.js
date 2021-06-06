3f7542ca14059868a8b6f0c44441c1a7635493a2
Fiamma no Jikukan <stavtxxx1@gmail.com>
2021-06-06 15:07:02 +0300

    it lives

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,12 +21,31 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/:date', (req, res) => {
+
+  console.log(req.params)
+
+  const { date } = req.params
+
+  const theDate = new Date(date);
+
+  const resultUnix = theDate.getTime();
+  const resultUTC = theDate.toUTCString();
+
+
+  return res.json({ "unix": resultUnix, UTC: resultUTC })
+})
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
